273e2290833a46b0fcc94677602ac1290ec0f0ab
Eric Mignot <eric.mignot@gmail.com>
2024-04-30 14:44:56 -0700

    wait for dynamodb via aws-cli

--- DynamoDB/wait/wait.sh
+++ DynamoDB/wait/wait.sh
@@ -3,7 +3,15 @@
 source '../support/waiting.sh'
 
 function with_this_waiting_command {
-    curl http://localhost:8000 | grep "MissingAuthenticationToken"
+    docker run \
+        --rm \
+        --network dynamodb \
+        -e AWS_ACCESS_KEY_ID=any \
+        -e AWS_SECRET_ACCESS_KEY=any \
+        amazon/aws-cli:2.15.42 dynamodb \
+        --endpoint http://dynamodb:8000 \
+        --region us-west-2 \
+        list-tables | grep "TableNames"
 }
 
 waiting dynamodb with_this_waiting_command
--- MongoDB/wait/wait.sh
+++ MongoDB/wait/wait.sh
@@ -3,7 +3,12 @@
 source '../support/waiting.sh'
 
 function with_this_waiting_command {
-    docker run --rm --network mongodb mongo:5.0.26 mongosh --host mongodb --eval "db.runCommand({ping:1})" | grep "ok"
+    docker run \
+        --rm \
+        --network mongodb \
+        mongo:5.0.26 mongosh \
+        --host mongodb \
+        --eval "db.runCommand({ping:1})" | grep "ok"
 }
 
 waiting mongodb with_this_waiting_command
